575b262b922159d9b2c3abee8f98cdb188970a20
Quan Tran <quanabcis2003@gmail.com>
2022-10-11 17:50:21 +0100

    Fixed max description length

--- things/models.py
+++ things/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, blank=False, unique=True)
-    description = models.CharField(max_length=30, blank=True, unique=False)
+    description = models.CharField(max_length=120, blank=True, unique=False)
     quantity = models.IntegerField(unique=False, validators=[
         MaxValueValidator(limit_value=100, message='Value must not be higher than 100.'),
         MinValueValidator(limit_value=0, message='Value must not be lower than 0.'),
