05ecdd5af1c225f9de130e8e42d7fe73263d5575
Quan Tran <quanabcis2003@gmail.com>
2022-10-11 17:47:51 +0100

    Fix typo

--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator
 from django.core.validators import MinValueValidator
 
-class thing(models.Model):
+class Thing(models.Model):
     name = models.CharField(max_length=30, blank=False, unique=True)
     description = models.CharField(max_length=30, blank=True, unique=False)
     quantity = models.IntegerField(unique=False, validators=[
